664d49b50846020f68cd18faa96fcf26784996ba
Patrick Herbers <patrick.herbers@rub.de>
2023-12-11 12:50:56 +0100

    Added Health bar and hurt boxes

---
+++ Assets/Scripts/HurtBox.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtBox : MonoBehaviour {
+    public int damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other is CharacterController && other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            var playerData = other.GetComponent<PlayerData>();
+            if(playerData == null)
+                return;
+            Debug.Log("Hurting the Player for " + damage + " damage", this);
+            playerData.Damage(damage);
+        }
+    }
+}
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,21 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerData : MonoBehaviour
 {
 
     public GameObject head;
+
+    public int maxHealth = 100;
+    private int _currentHealth;
+
+    // Damage binning only applies the maximum damage value over the damage binning time to avoid accumulated damage from multiple sources
+    public float damageBinningTime = 0.5f;
+    private int _damageBin;
+    private float _damageBinTimer;
+    public int CurrentHealth {
+        get => _currentHealth;
+        set => SetHealth(value);
+    }
+
+    // Events
+
+    public UnityEvent<int> OnHealthChanged;
+    public UnityEvent OnPlayerDeath;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_damageBinTimer > 0) {
+            _damageBinTimer -= Time.deltaTime;
+            if (_damageBinTimer <= 0) {
+                _damageBinTimer = 0;
+                _damageBin = 0;
+            }
+        }
+    }
+
+    // Hurt the player while respecting damage binning
+    // Only the maximum Damage value over the last half second is applied if damageBinning = true
+    public void Damage(int damage, bool damageBinning = true) {
+        if (damageBinning) {
+            if (_damageBinTimer > 0) {
+                if (damage > _damageBin) {
+                    ModHealth(-(damage - _damageBin));
+                    _damageBin = damage;
+                }
+            } else {
+                _damageBinTimer = damageBinningTime;
+                _damageBin = damage;
+                ModHealth(-damage);
+            }
+        } else {
+            ModHealth(-damage);
+        }
+    }
+
+    // Modify the player's health
+    // No damage binning is applied
+    public void ModHealth(int change) {
+        SetHealth(_currentHealth + change);
+    }
+
+    private void SetHealth(int newHealth) {
+        newHealth = Mathf.Clamp(newHealth, 0, maxHealth);
+        if (newHealth != _currentHealth) {
+            _currentHealth = newHealth;
+            OnHealthChanged.Invoke(_currentHealth);
+            Debug.Log("Player health now " + _currentHealth);
+            if (_currentHealth <= 0) {
+                Debug.Log("Player is dead");
+                OnPlayerDeath.Invoke();
+            }
+        }
     }
 }
