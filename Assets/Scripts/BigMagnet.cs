d0cb5c70435d55021d3c9092be553883f548ff66
Patrick Herbers <patrick.herbers@rub.de>
2023-12-12 18:46:49 +0100

    Added Magnet stops

--- Assets/Scripts/BigMagnet.cs
+++ Assets/Scripts/BigMagnet.cs
@@ -11,34 +11,60 @@ public class BigMagnet : MonoBehaviour {
     public float railPos;
     private float _railLength;
 
-    private float _rotationSpeed = 0.5f; 
+    private float _rotationSpeed = 0.5f;
+
+    public List<Transform> railStops = new();
+    private float[] _railStopPositions;
+    public int currentStop = 0;
     
     // Start is called before the first frame update
     void Start() {
         _railLength = rail.CalculateLength();
+        _railStopPositions = new float[railStops.Count];
+        for (var index = 0; index < railStops.Count; index++) {
+            var railStop = railStops [index];
+            SplineUtility.GetNearestPoint(rail.Spline, railStop.localPosition, out var nearest, out float f);
+            _railStopPositions[index] = f * _railLength;
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        railPos += railMoveSpeed * Time.deltaTime * railMove;
-        transform.position = rail.EvaluatePosition(railPos/_railLength);
-
-        var tangent = rail.EvaluateTangent(railPos / _railLength);
+    void Update() {
+        var targetRailPos = _railStopPositions [currentStop];
+        if (Mathf.Approximately(railPos, targetRailPos)) {
+            // We are at the current stop
+            railMove = 0;
+        } else if (railPos < targetRailPos) {
+            // Moving forward
+            railMove = 1;
+            railPos += railMoveSpeed * Time.deltaTime * railMove;
+            if (railPos > targetRailPos)
+                railPos = targetRailPos;
+        } else {
+            // Moving backward
+            railMove = -1;
+            railPos += railMoveSpeed * Time.deltaTime * railMove;
+            if (railPos < targetRailPos)
+                railPos = targetRailPos;
+        }
+        rail.Evaluate(railPos/_railLength, out var pos, out var tangent, out _);
+        transform.position = pos;
         var tangentVec = new Vector3(tangent.x, 0, tangent.z);
         var newDirection = Vector3.RotateTowards(transform.forward, tangentVec, _rotationSpeed * Time.deltaTime, 0.0f);
         transform.rotation = Quaternion.LookRotation(newDirection);
     }
 
     public void MoveForward() {
-        railMove = 1f;
+        currentStop++;
+        currentStop = Mathf.Clamp(currentStop, 0, _railStopPositions.Length);
     }
 
     public void MoveBackward() {
-        railMove = -1f;
+        currentStop--;
+        currentStop = Mathf.Clamp(currentStop, 0, _railStopPositions.Length);
     }
 
     public void Stop() {
-        railMove = 0f;
+        //railMove = 0f;
     }
 }
